065caf9de090bf9cd2c4a5f279930e5698de7429
Fingertips <ghiantan0418@gmail.com>
2024-08-31 17:15:56 +0800

    Add favicon

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import cookieParser from "cookie-parser";
+import favicon from "serve-favicon";
+import { fileURLToPath } from "url";
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import { Routes } from "./constants/routes.constant.js";
 import rootRoutes from "./routes/root.route.js";
@@ -12,9 +15,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Setup favicon
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__fileName);
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+
 app.use(express.json());
 app.use(cookieParser());
 
+// Setup routes
 app.use(Routes.root.path, rootRoutes);
 app.use(Routes.auth.path, authRoutes);
 
