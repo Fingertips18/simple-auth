163a0f51dc9fa482d23c3e8f1549a0277cfd124f
Fingertips <ghiantan0418@gmail.com>
2024-11-03 21:02:08 +0800

    fix: Check and set session verification in authGuard

--- frontend/src/guards/auth-guard.jsx
+++ frontend/src/guards/auth-guard.jsx
@@ -1,10 +1,36 @@
+import { useCallback, useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
+import { LoadingSpinner } from "@/components/loading-spinner";
+import { AuthService } from "@/lib/services/auth.service";
 import { useAuthStore } from "@/lib/stores/authStore";
 import { AppRoutes } from "@/constants/routes";
 
 const AuthGuard = () => {
-  const { authorized, user } = useAuthStore();
+  const { authorized, setAuthorized, user, setUser } = useAuthStore();
+  const [loading, setLoading] = useState(false);
+
+  const handleVerification = useCallback(async () => {
+    setLoading(true);
+
+    try {
+      const result = await AuthService.verifyToken();
+      setUser(result.user);
+      setAuthorized(true);
+    } catch {
+      setAuthorized(false);
+    } finally {
+      setLoading(false);
+    }
+  }, [setAuthorized, setUser, setLoading]);
+
+  useEffect(() => {
+    handleVerification();
+  }, [handleVerification]);
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
 
   if (authorized && user.isVerified) {
     return <Navigate to={AppRoutes.root} replace />;
